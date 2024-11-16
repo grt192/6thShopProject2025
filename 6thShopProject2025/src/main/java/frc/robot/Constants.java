a39106296e83d4003ad22ae77a222dfac212a6df
Tony Wu <tonyxywu@gmail.com>
2024-11-15 16:56:20 -0800

    pivot 'done'

--- 6thShopProject2025/src/main/java/frc/robot/Constants.java
+++ 6thShopProject2025/src/main/java/frc/robot/Constants.java
@@ -20,4 +20,19 @@ public final class Constants {
   public static class  RollerConstants {
     public static final int ROLLER_MOTOR_ID = 9;//placeholder value
   }
+
+  public static class  PivotConstants {
+    public static final int PIVOT_MOTOR_ID = 8;//placeholder value
+    public static final double PIVOT_P = 8;//placeholder value
+    public static final double PIVOT_I = 8;//placeholder value
+    public static final double PIVOT_D = 8;//placeholder value
+
+    public static final double CLOSED = 0;//placeholder value
+    public static final double FLOOR = 1;//placeholder value
+    public static final double DROPBOX = .5;//placeholder value
+
+
+
+  }
 }
+
