03bca605f4b6f25bd952c09eda26842a36c14cec
swaswa999 <swayams@icloud.com>
2024-11-20 10:57:29 -0800

    Belt Subsystem with sensors

--- 6thShopProject2025/src/main/java/frc/robot/RobotContainer.java
+++ 6thShopProject2025/src/main/java/frc/robot/RobotContainer.java
@@ -4,33 +4,44 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.TankSubsystem;
+import frc.robot.subsystems.BeltSubsystem;
 
 public class RobotContainer {
 
   // Subsystems defined
   private final TankSubsystem tankSubsystem;
+  private final BeltSubsystem beltSubsystem;
 
   // Controls defined
   private final CommandPS4Controller mechController;
   public double joyConLeft;
   public double joyConRight;
 
+  // sensors defined
+  private final DigitalInput beltSwitch;
+
   public RobotContainer() {
 
     // Subsystem created
     tankSubsystem = new TankSubsystem();
+    beltSubsystem = new BeltSubsystem();
 
     // Configure Bindings
     mechController = new CommandPS4Controller(0); // Use PS4 controller
     joyConLeft = 0;
     joyConRight = 0;
+
+    // Configure switch
+    beltSwitch = new DigitalInput(1); // enter belt switch number
 
     configureBindings();
   }
@@ -53,6 +64,21 @@ public class RobotContainer {
       this.joyConRight = mechController.getRightY(); // Right Y-axis for PS5 controller
       tankSubsystem.setMotors(joyConLeft, joyConRight);
     }, tankSubsystem));
+
+    mechController.square().onTrue(new InstantCommand(() -> {
+      if (beltSwitch.get()) {
+        beltSubsystem.breakRoller(true);
+        ;
+      } else {
+        beltSubsystem.setBelt(0.2);
+      }
+    }));
+    mechController.circle().onTrue(new InstantCommand(() -> {
+      beltSubsystem.breakRoller(false);
+      beltSubsystem.setBelt(0.2);
+
+    }));
+
   }
 
   // /**
---
+++ 6thShopProject2025/src/main/java/frc/robot/subsystems/BeltSubsystem.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.CANSparkMax;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class BeltSubsystem extends SubsystemBase {
+
+  // declaring each motor
+  private final WPI_TalonSRX mainBeltMotor;
+
+  /** Creates a new ExampleSubsystem. */
+  public BeltSubsystem() {
+
+    mainBeltMotor = new WPI_TalonSRX(3);
+    mainBeltMotor.setNeutralMode(NeutralMode.Coast); // turning braking to Coast
+
+  }
+
+  /**
+   * Sets the leftMotors and rightMotors speed and direction.
+   *
+   * @param speed The raw motor speed to set the rollers to (typically
+   *              between -1.0 and 1.0).
+   */
+  public void setBelt(double speed) {
+    mainBeltMotor.set(speed);
+
+  }
+
+  /**
+   * Sets the leftMotors and rightMotors brakes.
+   *
+   * @param motorBreak boolean for if break
+   */
+  public void breakRoller(boolean motorBreak) {
+    if (motorBreak) {
+      mainBeltMotor.setNeutralMode(NeutralMode.Brake);
+    } else if (!motorBreak) {
+      mainBeltMotor.setNeutralMode(NeutralMode.Coast);
+    }
+
+  }
+
+}
